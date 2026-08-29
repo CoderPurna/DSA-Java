8e01b86c4f68d25821353ce96876783c7956ecc5
PURNABRATA DEY <deypurnabrata2005@gmail.com>
2026-08-29 21:17:10 +0530

    Starting DSA in java

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+
+    //Numeric DT - short ,byte , int, long
+    static void main() {
+        byte num1 = 122;
+        System.out.println(num1);
+        short num2 = 0;
+        System.out.println(num2);
+        int num3 = 0;
+        System.out.println(num3);
+        long num4 = 0;
+        System.out.println(num4);
+
+        //floating DT
+        float num5 = 12.3f;
+        System.out.println(num5);
+
+        double num6 = 12.1551111111111111111d;
+        System.out.println(num6);
+
+        //other - char , boolean
+        boolean num7 = true;
+        System.out.println(num7);
+
+        char firstChar = 'a';
+        System.out.println((char) (firstChar+2)); //type casting
+    }
+
+
+}
---
+++ src/clasical.java
@@ -0,0 +1,5 @@
+public class clasical {
+    public void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
